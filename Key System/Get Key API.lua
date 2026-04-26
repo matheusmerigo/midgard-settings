296c6b40ce2843a725d90f858e201d3d32b14b5a
HELLHOUND <83030488+matheusmerigo@users.noreply.github.com>
2026-04-25 23:24:49 -0300

    Update Get Key API.lua

--- Key System/Get Key API.lua
+++ Key System/Get Key API.lua
@@ -3,12 +3,12 @@ return {
 
     [9186719164] = { -- Sailor Piece
         ["Linkvertise"] = {
-            Url = "https://ads.luarmor.net/get_key?for=Midgard-DaqFQyjrqCZy",
+            Url = "https://ads.luarmor.net/get_key?for=Midgard-NVketkFqfmrr",
             Icon = 138427217607086,
         },
 
         ["LootLabs"] = {
-            Url = "https://ads.luarmor.net/get_key?for=Midgard-NVketkFqfmrr",
+            Url = "https://ads.luarmor.net/get_key?for=Midgard-DaqFQyjrqCZy",
             Icon = 77347832178479,
         },
 
