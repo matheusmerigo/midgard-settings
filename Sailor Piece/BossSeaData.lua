f0dccd0cbae2d3b00d8074227b1f4a0c9dc337c0
HELLHOUND <83030488+matheusmerigo@users.noreply.github.com>
2026-04-28 22:47:25 -0300

    Create BossSeaData.lua

---
+++ Sailor Piece/BossSeaData.lua
@@ -0,0 +1,44 @@
+return {
+    Sea1 = {
+        PlaceIds = {
+            [130167267952199] = true,
+        },
+        Bosses = {
+            ["AizenBoss"] = { displayName = "Manipulator" },
+            ["AlucardBoss"] = { displayName = "Vampire King" },
+            ["AnosBoss"] = { displayName = "Demon King" },
+            ["AtomicBoss"] = { displayName = "Atomic" },
+            ["BlessedMaidenBoss"] = { displayName = "Blessed Maiden" },
+            ["GilgameshBoss"] = { displayName = "King of Heroes" },
+            ["GojoBoss"] = { displayName = "Limitless Sorcerer" },
+            ["GreatMageBoss"] = { displayName = "Great Mage" },
+            ["IceQueenBoss"] = { displayName = "Ice Queen" },
+            ["IchigoBoss"] = { displayName = "Soul Reaper" },
+            ["JinwooBoss"] = { displayName = "Solo Hunter" },
+            ["MadokaBoss"] = { displayName = "Magical Girl" },
+            ["MoonSlayerBoss"] = { displayName = "Moon Slayer" },
+            ["QinShiBoss"] = { displayName = "Qin Shi" },
+            ["RagnaBoss"] = { displayName = "Dragon Slayer" },
+            ["RimuruBoss"] = { displayName = "Demon Lord" },
+            ["SaberAlterBoss"] = { displayName = "Corrupted Knight" },
+            ["SaberBoss"] = { displayName = "Knight" },
+            ["StrongestinHistoryBoss"] = { displayName = "Strongest in History" },
+            ["StrongestofTodayBoss"] = { displayName = "Strongest of Today" },
+            ["StrongestShinobiBoss"] = { displayName = "Strongest Shinobi" },
+            ["SukunaBoss"] = { displayName = "Cursed King" },
+            ["TrueAizenBoss"] = { displayName = "True Manipulator" },
+            ["YamatoBoss"] = { displayName = "Yamato" },
+            ["YujiBoss"] = { displayName = "Cursed Vessel" },
+        },
+    },
+
+    Sea2 = {
+        PlaceIds = {
+            [77747658251236] = true,
+        },
+        Bosses = {
+            ["CosmicBeingBoss"] = { displayName = "Cosmic Being" },
+            ["TheWorldBoss"] = { displayName = "The World" },
+        },
+    },
+}
