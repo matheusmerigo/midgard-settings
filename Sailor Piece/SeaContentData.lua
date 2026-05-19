f9b6c4be676509c291de2bbcbf67a128c6831b04
HELLHOUND <83030488+matheusmerigo@users.noreply.github.com>
2026-05-19 14:05:49 -0300

    Update SeaContentData.lua

--- Sailor Piece/SeaContentData.lua
+++ Sailor Piece/SeaContentData.lua
@@ -1,5 +1,116 @@
 return {
     Sea1 = {
+        CenterMobs = {
+            "Thief4",
+            "Monkey2",
+            "DesertBandit2",
+            "FrostRogue3",
+            "Sorcerer3",
+            "Hollow2",
+            "StrongSorcerer4",
+            "Curse3",
+            "Slime3",
+            "AcademyTeacher3",
+            "Swordsman4",
+            "Quincy4",
+            "Ninja4",
+            "ArenaFighter2",
+        },
+        PuzzleData = {
+            Dungeon = {
+                { Island = "Starter", CFrame = CFrame.new(89.91290283203125, 8.486827850341797, -136.65316772460938) },
+                { Island = "Jungle", CFrame = CFrame.new(-396.15545654296875, -0.6447906494140625, 510.06793212890625) },
+                { Island = "Desert", CFrame = CFrame.new(-1055.7454833984375, 5.379585266113281, -306.6563415527344) },
+                { Island = "Snow", CFrame = CFrame.new(-313.6352844238281, -2.386168956756592, -1188.1343994140625) },
+                { Island = "Shibuya", CFrame = CFrame.new(1717.1011962890625, 138.3859100341797, -27.25783920288086) },
+                { Island = "HollowIsland", CFrame = CFrame.new(-685.6112060546875, 98.17619323730469, 1335.4005126953125) },
+            },
+            Slime = {
+                { Island = "Desert", CFrame = CFrame.new(-855.03564453125, -0.6689300537109375, -321.8644714355469) },
+                { Island = "Snow", CFrame = CFrame.new(-434.9467468261719, 20.94719886779785, -1183.7974853515625) },
+                { Island = "Starter", CFrame = CFrame.new(61.1317138671875, 32.33457946777344, -144.24493408203125) },
+                { Island = "Jungle", CFrame = CFrame.new(-583.7114868164062, 54.83659744262695, 317.2171325683594) },
+                { Island = "Shibuya", CFrame = CFrame.new(1744.80322265625, 6.8449883460998535, 494.5093994140625) },
+                { Island = "HollowIsland", CFrame = CFrame.new(-435.4044189453125, 23.017263412475586, 1399.24365234375) },
+                { Island = "Shinjuku", CFrame = CFrame.new(787.80126953125, 64.3216552734375, -2309.12841796875) },
+            },
+            Demonite = {
+                { Island = "Academy", CFrame = CFrame.new(1006.0069580078125, 11.749679565429688, 1130.03369140625) },
+                { Island = "Academy", CFrame = CFrame.new(920.7957153320312, 70.39314270019531, 1478.1302490234375) },
+            },
+            Hogyoku = {
+                { Island = "Snow", CFrame = CFrame.new(-424.5894470214844, 56.60246276855469, -1235.32568359375) },
+                { Island = "Shibuya", CFrame = CFrame.new(1636.708984375, 85.52067565917969, 247.29190063476562) },
+                { Island = "HollowIsland", CFrame = CFrame.new(-636.0084228515625, 22.549041748046875, 1206.2811279296875) },
+                { Island = "Shinjuku", CFrame = CFrame.new(648.0999755859375, 137.94454956054688, -2069.597412109375) },
+                { Island = "Slime", CFrame = CFrame.new(-1206.5228271484375, 30.884933471679688, 463.206787109375) },
+                { Island = "Judgement", CFrame = CFrame.new(-906.3980102539062, 15.729995727539062, -1260.247802734375) },
+            },
+            AncientFragment = {
+                { Island = "Starter", Spots = {
+                    -- Spot1
+                    CFrame.new(56.96540069580078, 1.5771000385284424, -358.34759521484375),
+                    -- Spot2
+                    CFrame.new(265.2940979003906, 98.8373031616211, -292.53399658203125),
+                }},
+                { Island = "Desert", Spots = {
+                    -- Spot3
+                    CFrame.new(-672.1392822265625, 30.478500366210938, -522.423583984375),
+                    -- Spot4
+                    CFrame.new(-782.0399780273438, 13.914400100708008, -296.0408935546875),
+                }},
+                { Island = "Snow", Spots = {
+                    -- Spot5
+                    CFrame.new(-495.035400390625, 0.7875999808311462, -1104.3232421875),
+                }},
+                { Island = "Shibuya", Spots = {
+                    -- Spot6
+                    CFrame.new(1819.384033203125, 142.13510131835938, -85.05500030517578),
+                    -- Spot7
+                    CFrame.new(1410.943359375, 189.5299072265625, 579.871826171875),
+                }},
+                { Island = "HollowIsland", Spots = {
+                    -- Spot8
+                    CFrame.new(-601.1466064453125, 2.016200065612793, 954.6491088867188),
+                    -- Spot9
+                    CFrame.new(-826.7999877929688, 52.89910125732422, 1178.84619140625),
+                }},
+                { Island = "Shinjuku", Spots = {
+                    -- Spot10
+                    CFrame.new(627.4483032226562, 71.20059967041016, -2379.41455078125),
+                    -- Spot11
+                    CFrame.new(587.78369140625, 208.03160095214844, -2135.7900390625),
+                }},
+                { Island = "Slime", Spots = {
+                    -- Spot12
+                    CFrame.new(-1108.8333740234375, 13.650300025939941, 146.8509979248047),
+                    -- Spot13
+                    CFrame.new(-1430.563720703125, 3.902100086212158, 397.5195007324219),
+                }},
+                { Island = "Academy", Spots = {
+                    -- Spot14
+                    CFrame.new(881.2874145507812, 38.27939987182617, 1286.154052734375),
+                    -- Spot15
+                    CFrame.new(1057.2890625, 94.32489776611328, 1481.1007080078125),
+                }},
+                { Island = "Judgement", Spots = {
+                    -- Spot16
+                    CFrame.new(-936.4058227539062, 91.44999694824219, -1218.4913330078125),
+                    -- Spot17
+                    CFrame.new(-1573.510009765625, 38.57419967651367, -1418.109619140625),
+                }},
+                { Island = "Ninja", Spots = {
+                    -- Spot18
+                    CFrame.new(-1744.99755859375, 82.59590148925781, -749.609375),
+                }},
+                { Island = "Lawless", Spots = {
+                    -- Spot19
+                    CFrame.new(20.533899307250977, -5.756700038909912, 2079.89990234375),
+                    -- Spot20
+                    CFrame.new(-56.36280059814453, 6.808800220489502, 1826.66015625),
+                }},
+            },
+        },
         Bosses = {
 
             ["AizenBoss"] = { displayName = "Manipulator" },
@@ -53,6 +164,51 @@ return {
     },
 
     Sea2 = {
+        CenterMobs = {
+            "Delinquent1",
+            "StrongFighter3",
+            "StrongBandit5",
+            "FastNinja4",
+            "SpiritFighter5",
+            "StrongSlayer5",
+            "GhoulMember5",
+        },
+        PuzzleData = {
+            BloodFlower = {
+                { Island = "StarterSea2", CFrame = CFrame.new(-437.43304443359375, 85.17733001708984, -733.7615966796875) },
+                { Island = "Bizarre", CFrame = CFrame.new(-3290.145263671875, 16.85352325439453, -1107.9542236328125) },
+                { Island = "Punch", CFrame = CFrame.new(-1430.5562744140625, 12.470925331115723, 1710.2391357421875) },
+                { Island = "Sea2World", CFrame = CFrame.new(-195.57350158691406, -3.190446376800537, -3169.40576171875) },
+                { Island = "Slayer", CFrame = CFrame.new(-2337.03955078125, 99.50860595703125, -3244.47607421875) },
+                { Island = "BluePlanet", CFrame = CFrame.new(-3664.632080078125, 72.17731475830078, 1351.2432861328125) },
+            },
+            ArchangelHalo = {
+                {
+                    Island = "BluePlanet",
+                    SpawnName = "HaloSpawn1",
+                    SpawnedName = "AngelHalo_Spawn1",
+                    CFrame = CFrame.new(-3489.4384765625, 7.880629062652588, 874.1835327148438),
+                },
+                {
+                    Island = "Punch",
+                    SpawnName = "HaloSpawn2",
+                    SpawnedName = "AngelHalo_Spawn2",
+                    CFrame = CFrame.new(-1456.36865234375, 129.11549377441406, 1716.6353759765625),
+                },
+                {
+                    Island = "Bizarre",
+                    SpawnName = "HaloSpawn3",
+                    SpawnedName = "AngelHalo_Spawn3",
+                    CFrame = CFrame.new(-3195.500244140625, 35.125999450683594, -1025.01318359375),
+                },
+                {
+                    Island = "StarterSea2",
+                    SpawnName = "HaloSpawn4",
+                    SpawnedName = "AngelHalo_Spawn4",
+                    CFrame = CFrame.new(-561.3780517578125, 34.722938537597656, -220.97259521484375),
+                },
+            },
+        },
         Bosses = {
 
             ["TheWorldBoss"] = { displayName = "The World" },       
